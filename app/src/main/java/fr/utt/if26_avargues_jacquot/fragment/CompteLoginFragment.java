6ec1d75b28f725bda6357540c790e4531cf3ca00
Etienne <etiennej70@hotmail.fr>
2015-11-27 23:25:46 +0100

    Test bouton login. Préparation implémentation web service de vérif login.

--- app/src/main/java/fr/utt/if26_avargues_jacquot/fragment/CompteLoginFragment.java
+++ app/src/main/java/fr/utt/if26_avargues_jacquot/fragment/CompteLoginFragment.java
@@ -6,21 +6,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.guillaume.if26_avargues_jacquot.R;
+
+import fr.utt.if26_avargues_jacquot.webservices.LoginService;
 
 /**
  * Created by guillaume on 26/11/2015.
  */
-public class CompteLoginFragment extends Fragment {
-
+public class CompteLoginFragment extends Fragment implements View.OnClickListener {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onClick(View view) {
 
-        View rootView = inflater.inflate(R.layout.fragment_compte_login, container, false);
+        switch (view.getId()) {
+            case R.id.BT_validation:
+                    /* TODO
+                    * Récupérer les login et password tapés par l'utilisateur dans les champs de texte
+                    * */
+                    String login = "test";
+                    String passwd = "test";
+                LoginService loginService = new LoginService();
+                if(loginService.validateLogin(login, passwd) == true) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Vous avez cliqué sur le bouton Me." , Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getActivity().getApplicationContext(), "Erreur durant le login." , Toast.LENGTH_LONG).show();
+                }
 
-        return rootView;
+                break;
+
+        }
+    }
+
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_compte_login, container, false);
+        //On récupere la vue souhaitée et on lui affecte le Listener
+        view.findViewById(R.id.BT_validation).setOnClickListener(this);
+        return view;
     }
 }
---
+++ app/src/main/java/fr/utt/if26_avargues_jacquot/webservices/LoginService.java
@@ -0,0 +1,16 @@
+package fr.utt.if26_avargues_jacquot.webservices;
+
+/**
+ * Created by EtienneJ on 27/11/2015.
+ */
+public class LoginService {
+
+    public LoginService() {
+
+    }
+
+    public boolean validateLogin (String user,String passwd) {
+        return true;
+    }
+
+}
