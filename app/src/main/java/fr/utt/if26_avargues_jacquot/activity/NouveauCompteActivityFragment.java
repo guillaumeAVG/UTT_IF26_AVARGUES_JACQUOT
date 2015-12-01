d55c9f50126fe220362046fe49b0d5f2bbc204dc
Etienne <etiennej70@hotmail.fr>
2015-12-01 13:37:14 +0100

    Ajout activité "Nouveau compte"

---
+++ app/src/main/java/fr/utt/if26_avargues_jacquot/activity/NouveauCompteActivityFragment.java
@@ -0,0 +1,24 @@
+package fr.utt.if26_avargues_jacquot.activity;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.guillaume.if26_avargues_jacquot.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NouveauCompteActivityFragment extends Fragment {
+
+    public NouveauCompteActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_nouveau_compte, container, false);
+    }
+}
--- app/src/main/java/fr/utt/if26_avargues_jacquot/fragment/CompteLoginFragment.java
+++ app/src/main/java/fr/utt/if26_avargues_jacquot/fragment/CompteLoginFragment.java
@@ -1,6 +1,7 @@
 package fr.utt.if26_avargues_jacquot.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +11,8 @@ import android.widget.Toast;
 
 import com.example.guillaume.if26_avargues_jacquot.R;
 
+import fr.utt.if26_avargues_jacquot.activity.MainActivity;
+import fr.utt.if26_avargues_jacquot.activity.NouveauCompteActivity;
 import fr.utt.if26_avargues_jacquot.webservices.LoginService;
 
 /**
@@ -22,23 +25,31 @@ public class CompteLoginFragment extends Fragment implements View.OnClickListene
 
         switch (view.getId()) {
             case R.id.BT_validation:
-                    /* TODO
-                    * Récupérer les login et password tapés par l'utilisateur dans les champs de texte
-                    * */
-                String login = "test";
-                String passwd = "test";
-                LoginService loginService = new LoginService();
-                if (loginService.validateLogin(login, passwd) == true) {
-                    Toast.makeText(getActivity().getApplicationContext(), "Vous avez cliqué sur le bouton Me.", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getActivity().getApplicationContext(), "Erreur durant le login.", Toast.LENGTH_LONG).show();
-                }
-
+                onLoginButtonClick(view);
                 break;
-            case R.id.BT_ajouterUnBonPlan:
+            case R.id.BT_noCompte:
+                onNewAccountClick(view);
                 break;
 
         }
+    }
+
+    public void onLoginButtonClick(View view) {
+        /* TODO Récupérer les login et password tapés par l'utilisateur dans les champs de texte
+        */
+        String login = "test";
+        String passwd = "test";
+        LoginService loginService = new LoginService();
+        if (loginService.validateLogin(login, passwd) == true) {
+            Toast.makeText(getActivity().getApplicationContext(), "Vous avez cliqué sur le bouton Me.", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getActivity().getApplicationContext(), "Erreur durant le login.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onNewAccountClick(View view) {
+        Intent intent = new Intent(getActivity(), NouveauCompteActivity.class);
+        startActivity(intent);
     }
 
 
@@ -46,6 +57,7 @@ public class CompteLoginFragment extends Fragment implements View.OnClickListene
         View view = inflater.inflate(R.layout.fragment_compte_login, container, false);
         //On récupere la vue souhaitée et on lui affecte le Listener
         view.findViewById(R.id.BT_validation).setOnClickListener(this);
+        view.findViewById(R.id.BT_noCompte).setOnClickListener(this);
         return view;
     }
 }
