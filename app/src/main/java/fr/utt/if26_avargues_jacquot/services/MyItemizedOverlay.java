13d8052da1ec447b651d296fbf1ddc54e31f40d8
Etienne <etiennej70@hotmail.fr>
2015-12-24 12:56:08 +0100

    Récupération d'informations au clic sur un bon plan

--- app/src/main/java/fr/utt/if26_avargues_jacquot/services/MyItemizedOverlay.java
+++ app/src/main/java/fr/utt/if26_avargues_jacquot/services/MyItemizedOverlay.java
@@ -2,12 +2,16 @@ package fr.utt.if26_avargues_jacquot.services;
 
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.overlay.ItemizedOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
 import java.util.ArrayList;
+
+import fr.utt.if26_avargues_jacquot.activity.MainActivity;
 
 /**
  * Classe permettant de gérer l'overlay par dessus la carte permettant d'afficher les bons plans sur la carte.
@@ -44,7 +48,15 @@ public class MyItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 
 
     @Override
-    protected boolean onTap(int index) {
-        return super.onTap(index);
+    protected boolean onTap(int i) {
+        GeoPoint  gpoint = (GeoPoint) overlayItemList.get(i).getPoint();
+        double lat = gpoint.getLatitudeE6()/1e6;
+        double lon = gpoint.getLongitudeE6()/1e6;
+        String toast = "Title: "+overlayItemList.get(i).getTitle();
+        toast += "\nText: "+overlayItemList.get(i).getSnippet();
+        toast += 	"\nSymbol coordinates: Lat = "+lat+" Lon = "+lon+" (microdegrees)";
+        MainActivity main = new MainActivity();
+        Toast.makeText(main.ma.getApplicationContext(), toast, Toast.LENGTH_LONG).show();
+        return(true);
     }
 }
