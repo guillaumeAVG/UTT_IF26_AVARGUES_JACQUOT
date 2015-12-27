d51e14b8df5841f040646b76394aab5dd153c504
Etienne <etiennej70@hotmail.fr>
2015-12-27 18:13:49 +0100

    Possibilité de création de compte sur l'application

--- app/src/main/java/fr/utt/if26_avargues_jacquot/fragment/NouveauCompteActivityFragment.java
+++ app/src/main/java/fr/utt/if26_avargues_jacquot/fragment/NouveauCompteActivityFragment.java
@@ -4,14 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.guillaume.if26_avargues_jacquot.R;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import fr.utt.if26_avargues_jacquot.activity.MainActivity;
+import fr.utt.if26_avargues_jacquot.services.SignInService;
 
 /**
  * Created by guillaume on 11/12/2015.
@@ -21,11 +30,34 @@ Elle hérite de Fragment et implémente View.OnClickListener car cet écran poss
 
 public class NouveauCompteActivityFragment extends Fragment implements View.OnClickListener {
 
+    EditText nom;
+    EditText prenom;
+    EditText dateNaissance;
+    EditText adresseEmail;
+    RadioGroup choixEcole;
+    EditText numeroTelephone;
+    EditText motDePasse;
+    EditText motDePasseConfirm;
+
+
     /* La méthode onClick permet de définir une action dès que l'utilisateur clique sur le bouton valider ou annuler ou ajouter une image de profil.*/
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.BT_valider:
-                Toast.makeText(getActivity().getApplicationContext(), "Vous avez validé les informations", Toast.LENGTH_LONG).show();
+                Boolean boolAddCompte = false;
+                try {
+                    boolAddCompte = addCompte();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                if(boolAddCompte) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Le compte a été ajouté. Vous pouvez vous connecter", Toast.LENGTH_LONG).show();
+                    Intent intent2 = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent2);
+                }
                 break;
 
             case R.id.BT_annuler:
@@ -49,6 +81,81 @@ public class NouveauCompteActivityFragment extends Fragment implements View.OnCl
         view.findViewById(R.id.BT_valider).setOnClickListener(this);
         view.findViewById(R.id.BT_annuler).setOnClickListener(this);
         view.findViewById(R.id.BT_ajouterUneImage).setOnClickListener(this);
+
+        nom = (EditText) view.findViewById(R.id.TF_nouveauCompteNom);
+        prenom = (EditText) view.findViewById(R.id.TF_nouveauComptePrenom);
+        dateNaissance = (EditText) view.findViewById(R.id.TF_nouveauCompteDateNaissance);
+        adresseEmail = (EditText) view.findViewById(R.id.TF_nouveauCompteEmail);
+        choixEcole = (RadioGroup) view.findViewById(R.id.RG_NouveauCompteEcole);
+        numeroTelephone = (EditText) view.findViewById(R.id.TF_nouveauCompteTelephone);
+        motDePasse = (EditText) view.findViewById(R.id.TF_nouveauCompteMotdePasse);
+        motDePasseConfirm = (EditText) view.findViewById(R.id.TF_nouveauCompteMotdePasseConfirm);
+
         return view;
+    }
+
+    private boolean addCompte() throws IOException, JSONException {
+
+        String StringNom = nom.getText().toString();
+        String StringPrenom = prenom.getText().toString();
+        String StringDateNaissance = dateNaissance.getText().toString();
+        String StringAdresseEmail = adresseEmail.getText().toString();
+        String StringEcole = getChoixEcole();
+        String StringTelephone = numeroTelephone.getText().toString();
+        Integer IntTelephone;
+        if(StringTelephone.length() == 0) {IntTelephone = 0;}
+        else { IntTelephone = Integer.valueOf(StringTelephone); }
+
+        String StringMotDePasse = motDePasse.getText().toString();
+
+        if(StringPrenom.length() == 0 || StringNom.length() == 0 || StringDateNaissance.length() == 0 || StringAdresseEmail.length() == 0 || StringEcole.length() == 0 || StringMotDePasse.length() == 0) {
+            Toast.makeText(getActivity().getApplicationContext(), "Veuillez compléter tous les champs", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(!StringMotDePasse.equals(motDePasseConfirm.getText().toString())) {
+            Toast.makeText(getActivity().getApplicationContext(), "Les mots de passe ne correspondent pas", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(IntTelephone.toString().length() != 10 && IntTelephone != 0 ) {
+            Toast.makeText(getActivity().getApplicationContext(), "Erreur de numéro de téléphone", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        SignInService sis = new SignInService();
+        String addAccountResponse = sis.addAccount(StringNom, StringPrenom, StringDateNaissance, StringAdresseEmail, StringEcole, IntTelephone, StringMotDePasse);
+
+        if(addAccountResponse.equals("Already registered")) {
+            Toast.makeText(getActivity().getApplicationContext(), "Ce compte existe déjà", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if (addAccountResponse.equals("Success")){
+            return true;
+        }
+        else {
+            Toast.makeText(getActivity().getApplicationContext(), "Une erreur s'est produite. Merci de réessayer.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+    private String getChoixEcole() {
+        String retour = null;
+        switch (choixEcole.getCheckedRadioButtonId()) {
+            case R.id.RF_UTT:
+                retour = "UTT";
+                break;
+            case R.id.RF_EPF:
+                retour = "EPF";
+                break;
+            case R.id.RF_ESC:
+                retour = "ESC";
+                break;
+            case R.id.RF_IUT:
+                retour = "IUT";
+                break;
+            case R.id.RF_URCA:
+                retour = "URCA";
+                break;
+        }
+        return retour;
     }
 }
---
+++ app/src/main/java/fr/utt/if26_avargues_jacquot/services/SignInService.java
@@ -0,0 +1,107 @@
+package fr.utt.if26_avargues_jacquot.services;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.StrictMode;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import fr.utt.if26_avargues_jacquot.activity.MainActivity;
+
+/**
+ * Created by EtienneJ on 27/12/2015.
+ */
+public class SignInService {
+
+    /**
+     * URL du webservice
+     */
+    protected final URL urlToRequest;
+
+    /**
+     * Constructeur
+     *
+     * @throws MalformedURLException
+     */
+    public SignInService() throws MalformedURLException {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        this.urlToRequest = new URL("http://51.254.37.59/StudentN3_WS/register.php");
+    }
+
+
+    public String addAccount(String nom, String prenom, String dateNaissance, String email, String ecole, Integer telephone, String motdepasse) throws IOException, JSONException {
+
+        HttpURLConnection urlConnection = (HttpURLConnection) urlToRequest.openConnection();
+
+        String params = URLEncoder.encode("nom", "UTF-8")
+                + "=" + URLEncoder.encode(nom, "UTF-8");
+
+        params += "&" + URLEncoder.encode("prenom", "UTF-8") + "="
+                + URLEncoder.encode(prenom, "UTF-8");
+
+        params += "&" + URLEncoder.encode("dateNaissance", "UTF-8") + "="
+                + URLEncoder.encode(dateNaissance, "UTF-8");
+
+        params += "&" + URLEncoder.encode("email", "UTF-8") + "="
+                + URLEncoder.encode(email, "UTF-8");
+
+        params += "&" + URLEncoder.encode("ecole", "UTF-8") + "="
+                + URLEncoder.encode(ecole, "UTF-8");
+
+        params += "&" + URLEncoder.encode("telephone", "UTF-8") + "="
+                + URLEncoder.encode(telephone.toString(), "UTF-8");
+
+        params += "&" + URLEncoder.encode("motdepasse", "UTF-8") + "="
+                + URLEncoder.encode(motdepasse, "UTF-8");
+
+
+
+        urlConnection.setDoOutput(true);
+        OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+        wr.write(params);
+        wr.flush();
+
+        InputStream is;
+
+        is = urlConnection.getInputStream();
+        int ch;
+        StringBuffer sb = new StringBuffer();
+        while ((ch = is.read()) != -1) {
+            sb.append((char) ch);
+        }
+
+        String response = sb.toString();
+
+        JSONObject jsonResponse = new JSONObject(response);
+        String messageResponse = jsonResponse.getString("message");
+
+        switch (messageResponse) {
+            case "No POST":
+            case "Missing params":
+            case "Telephone length error":
+            case "No valide email":
+                Log.d("Webservice error", messageResponse);
+                return "Internal error";
+            case "Already registered":
+                return "Already registered";
+            case "Success":
+                return "Success";
+            default:
+                return "Undefined error";
+        }
+    }
+}
